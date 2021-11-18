274b5d0459f47058ee482a42b0dfcb2e992a0961
fgk244 <fgk.244@gmail.com>
2021-11-18 03:16:15 +0000

    18commit

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,2 +1,30 @@
 class CategoriesController < ApplicationController
+
+  def create
+    @category = Category.new(category_params)
+    @category.save
+    redirect_to categories_path
+  end
+
+  def index
+    @newcategory = Category.new
+    @categories = Category.all
+  end
+
+  def edit
+    @category = Category.find(params[:id])
+  end
+
+  def update
+    @category = Category.find(params[:id])
+    @category.update(category_params)
+    redirect_to categories_path
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:category_name)
+  end
+
 end
